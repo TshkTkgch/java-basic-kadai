b0c7e32323dcea34380aa47d19c3247f0add4731
TshkTkgch <chocolataulait7140@gmail.com>
2025-05-02 21:46:05 +0900

    修正

--- java-basic-kadai/src/kadai_018/KatoExec_Chapter18.java
+++ java-basic-kadai/src/kadai_018/KatoExec_Chapter18.java
@@ -1,7 +1,7 @@
 package kadai_018;
 
 public class KatoExec_Chapter18 {
-	public static void main(String args) {
+	public static void main(String[] args) {
 		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
 		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
 		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
